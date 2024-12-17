5f3556298d7eafaf2ed6575acf19886afc67e75a
melked <ce.melikedemirtas@gmail.com>
2024-12-18 00:21:22 +0300

    Initial commit

---
+++ KuaforUygulamasi/Controllers/RandevuController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KuaforUygulamasi.Models;
+
+namespace KuaforUygulamasi.Controllers
+{
+    public class RandevuController : Controller
+    {
+        // Geçici bir liste ile veri tutma
+        private static List<Randevu> randevular = new List<Randevu>();
+
+        // Randevuları listeleyen Action
+        public IActionResult Index()
+        {
+            return View(randevular);
+        }
+
+        // Yeni randevu ekleme formu
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Yeni randevuyu ekleyen Action (POST)
+        [HttpPost]
+        public IActionResult Create(Randevu randevu)
+        {
+            if (ModelState.IsValid)
+            {
+                randevu.Id = randevular.Count + 1;
+                randevular.Add(randevu);
+                return RedirectToAction("Index");
+            }
+            return View(randevu);
+        }
+
+        // Randevuyu silen Action
+        public IActionResult Delete(int id)
+        {
+            var randevu = randevular.FirstOrDefault(r => r.Id == id);
+            if (randevu != null)
+            {
+                randevular.Remove(randevu);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ KuaforUygulamasi/Models/randevu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace KuaforUygulamasi.Models
+{
+    public class Randevu
+    {
+        public int Id { get; set; }
+        public string MusteriAdi { get; set; }
+        public DateTime Tarih { get; set; }
+        public string HizmetTuru { get; set; } // Saç kesimi, boyama, bakım vb.
+    }
+}
+
