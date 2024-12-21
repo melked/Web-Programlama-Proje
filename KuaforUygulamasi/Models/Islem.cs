c437398ac70fcecd69b408c1bc08429b405870f1
Yasin Can Kaya <ce.yasincankaya@gmail.com>
2024-12-21 16:32:54 +0300

    Migrations eklendi.

--- KuaforUygulamasi/Models/Islem.cs
+++ KuaforUygulamasi/Models/Islem.cs
@@ -1,10 +1,14 @@
-﻿namespace KuaforUygulamasi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KuaforUygulamasi.Models
 {
     public class Islem
     {
         public int ID { get; set; }
         public string Ad { get; set; }
         public int Sure { get; set; } // Dakika cinsinden
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Ucret { get; set; }
 
     }
